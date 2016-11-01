fcb7109f07e091bcd98027a0c65cf00ebf6a5bff
Marie Eriksson <eriksson.km@gmail.com>
2016-11-01 21:24:06 +0100

    rearrange in js file and try to count turned cards.

--- memorygame.js
+++ memorygame.js
@@ -1,38 +1,57 @@
 'use strict';
 
-// loop out 16 cards to memoryboard
-for (let i = 0; i < 16; i++) {
-	// creating back of memory card and add to memoryboard
-  let card = document.createElement('div');
-  card.className = 'backofcard';
-  document.querySelector('.memoryboard').appendChild(card);
-
-	// test flip card to front with given color
-  card.addEventListener('click', function (event) {
-    this.className = 'front';
-		this.innerHTML = "<img src='images/" + cardImages[i] + ".png'>";
-  });
-}
- // memory card images as objects
+ // Here's my 16 memory cards stored in an array (value equals img file name)
 const cardImages = ['chase', 'chase', 'everest', 'everest', 'marshall', 'marshall', 'rocky', 'rocky', 'rubble', 'rubble', 'ryder', 'ryder', 'skye', 'skye', 'zuma', 'zuma'];
 
-// get and rewrite shuffle
+// Let's shuffle the memory cards using Fisher–Yates shuffle
 function shuffle (array) {
-  var m = array.length, t, i;
-
+  let m = array.length, t, i;
   // While there remain elements to shuffle…
   while (m) {
-
     // Pick a remaining element…
     i = Math.floor(Math.random() * m--);
-
     // And swap it with the current element.
     t = array[m];
     array[m] = array[i];
     array[i] = t;
   }
-
   return array;
 }
 shuffle(cardImages);
 console.log(cardImages);
+
+// Then put all the cards on the game board with the back facing up
+for (let i = 0; i < 16; i++) {
+	// creating html div as memory card
+  let card = document.createElement('div');
+  card.className = 'backofcard';
+  document.querySelector('.memoryboard').appendChild(card);
+
+	// flip card to front when clicked
+  card.addEventListener('click', function (event) {
+    this.className = 'front';
+    this.innerHTML = "<img src='images/" + cardImages[i] + ".png'>";
+  });
+}
+
+let clicks = 0;
+const getCards = document.querySelectorAll('.backofcard');
+getCards.forEach (function(cardClicks) {
+	cardClicks.addEventListener('click', function(event) {
+		clicks++;
+		if (clicks == 2) {
+			let frontCards = document.querySelectorAll('.front img');
+			if (frontCards[0].src == frontCards[1].src) {
+				console.log('par!');
+				clicks = 0;
+			}
+		}
+	});
+});
+
+
+//function countClicks ()
+// new loop - count clicks, if two cards cliked - matching. reset count click.
+// if not matching - set all cards to class backofcard.
+// when all cards have class front - game finished
+// replay game with button by calling shufflefunction and set all cards to class backofcard
